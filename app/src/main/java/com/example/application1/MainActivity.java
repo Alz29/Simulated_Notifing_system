74af1fe500668e5b3a67804716c90e5dc4f57049
Alz29 <2017512@brunel.ac.uk>
2023-02-06 23:31:35 +0000

    Changing font and style to become suitable for vulnerbale people making:  >The text simple  >Contrasting colours  >Font readable and large  > Easily able to navigate

--- app/src/main/java/com/example/application1/MainActivity.java
+++ app/src/main/java/com/example/application1/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.application1;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,13 +17,23 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
     public static FirebaseFirestore fireStore = FirebaseFirestore.getInstance();
-
+    Button res_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         readdata();
+        res_button= (Button) findViewById(R.id.Resbutton);
+        res_button.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+            Intent intent = new Intent();
+            intent.setClass(getApplicationContext(),Schedule.class);
+            startActivity(intent);
+
+            }
+        });
     }
     public void readdata(){
         fireStore.collection("Devices")
