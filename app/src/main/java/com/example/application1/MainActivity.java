de42003ee0276e59cd9043554d1eef4c89522e32
Alz29 <2017512@brunel.ac.uk>
2023-02-06 23:06:14 +0000

    Successfully connected the database, ready to query

--- app/src/main/java/com/example/application1/MainActivity.java
+++ app/src/main/java/com/example/application1/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.application1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
+    public static FirebaseFirestore fireStore = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        readdata();
+
+    }
+    public void readdata(){
+        fireStore.collection("Devices")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                //when the data reach's this point what do you want to do? Check the isLocked == "true" (for example)
+                                Toast.makeText (getApplicationContext(),"Successful", Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Toast.makeText (getApplicationContext(),"UnSuccessful", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
